0f71a0dc70afd5909a4a64ff05d52c0211ab3258
awinardi1004 <awinardi1004@gmail.com>
2025-02-22 19:46:21 +0700

    S3 storage implemented

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -1,28 +1,28 @@
-const ClientError = require('../../exceptions/ClientError');
- 
+
 class UploadsHandler {
     constructor(service, validator) {
-        this._service = service;
-        this._validator = validator;
-
-        this.postUploadImageHandler = this.postUploadImageHandler.bind(this);
+      this._service = service;
+      this._validator = validator;
+  
+      this.postUploadImageHandler = this.postUploadImageHandler.bind(this);
     }
-    
+  
     async postUploadImageHandler(request, h) {
-        const { data } = request.payload;
-        this._validator.validateImageHeaders(data.hapi.headers);
-        
-        const filename = await this._service.writeFile(data, data.hapi);
-        
-        const response = h.response({
-            status: 'success',
-            data: {
-                fileLocation: `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`,
-            },
-        });
-        response.code(201);
-        return response;
+      const { data } = request.payload;
+      this._validator.validateImageHeaders(data.hapi.headers);
+  
+      const fileLocation = await this._service.writeFile(data, data.hapi);
+  
+      const response = h.response({
+        status: 'success',
+        data: {
+          fileLocation,
+        },
+      });
+      response.code(201);
+      return response;
     }
-}
- 
-module.exports = UploadsHandler;
+  }
+  
+  module.exports = UploadsHandler;
+  
